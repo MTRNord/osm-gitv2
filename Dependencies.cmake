f101ba63c0ff712a4429b25438c49581cb238113
MTRNord <mtrnord1@gmail.com>
2024-02-12 17:16:06 +0100

    Make compile with basic cli setup

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -9,7 +9,7 @@ function(osm_git_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET fmtlib::fmtlib)
-    cpmaddpackage("gh:fmtlib/fmt#9.1.0")
+    cpmaddpackage("gh:fmtlib/fmt#10.2.1")
   endif()
 
   if(NOT TARGET spdlog::spdlog)
@@ -17,7 +17,7 @@ function(osm_git_setup_dependencies)
       NAME
       spdlog
       VERSION
-      1.11.0
+      1.13.0
       GITHUB_REPOSITORY
       "gabime/spdlog"
       OPTIONS
@@ -25,11 +25,20 @@ function(osm_git_setup_dependencies)
   endif()
 
   if(NOT TARGET Catch2::Catch2WithMain)
-    cpmaddpackage("gh:catchorg/Catch2@3.3.2")
+    cpmaddpackage("gh:catchorg/Catch2@3.5.2")
   endif()
 
   if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
+    cpmaddpackage("gh:CLIUtils/CLI11@2.4.1")
+  endif()
+
+  if(NOT TARGET libgit2cpp::git2cpp)
+    cpmaddpackage(
+      NAME libgit2cpp
+      GITHUB_REPOSITORY AndreyG/libgit2cpp
+      GIT_TAG master
+      OPTIONS
+        "BUILD_LIBGIT2CPP_EXAMPLES OFF")
   endif()
 
 endfunction()
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,28 @@
-int main() { return 0; }
+#include <CLI/CLI.hpp>
+#include <spdlog/spdlog.h>
+
+// This file will be generated automatically when you run the CMake
+// configuration step. It creates a namespace called `osm_git`. You can modify
+// the source template at `configured_files/config.hpp.in`.
+#include <internal_use_only/config.hpp>
+
+// NOLINTNEXTLINE(bugprone-exception-escape)
+int main(int argc, char **argv)
+{
+  try {
+    CLI::App app{ "A tool which converts regular osm files into yaml based git repos" };
+    argv = app.ensure_utf8(argv);
+
+    bool show_version = false;
+    app.add_flag("--version", show_version, "Show version information");
+    CLI11_PARSE(app, argc, argv);
+
+    if (show_version) {
+      fmt::print("{}\n", osm_git::cmake::project_version);
+      return EXIT_SUCCESS;
+    }
+  } catch (const std::exception &e) {
+    spdlog::error("Unhandled exception in main: {}", e.what());
+  }
+  return EXIT_SUCCESS;
+}
